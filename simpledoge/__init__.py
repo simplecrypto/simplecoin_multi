9261bd841e4414ba48372d06599b76b49b7b1bf3
Isaac Cook <isaac@simpload.com>
2014-02-24 20:36:26 -0600

    Added a time ago filter for use in templates

--- simpledoge/__init__.py
+++ simpledoge/__init__.py
@@ -4,6 +4,7 @@ from flask.ext.cache import Cache
 from jinja2 import FileSystemLoader
 from werkzeug.local import LocalProxy
 from bitcoinrpc import AuthServiceProxy
+from datetime import datetime
 
 import logging
 import six
@@ -50,6 +51,50 @@ def create_app(config='/config.yml'):
     # register all our plugins
     db.init_app(app)
     cache.init_app(app, config={'CACHE_TYPE': 'simple'})
+
+    # filters for jinja
+    @app.template_filter('time_ago')
+    def pretty_date(time=False):
+        """
+        Get a datetime object or a int() Epoch timestamp and return a
+        pretty string like 'an hour ago', 'Yesterday', '3 months ago',
+        'just now', etc
+        """
+        now = datetime.now()
+        if type(time) is int:
+            diff = now - datetime.fromtimestamp(time)
+        elif isinstance(time, datetime):
+            diff = now - time
+        elif not time:
+            diff = now - now
+        second_diff = diff.seconds
+        day_diff = diff.days
+
+        if day_diff < 0:
+            return ''
+
+        if day_diff == 0:
+            if second_diff < 10:
+                return "just now"
+            if second_diff < 60:
+                return str(second_diff) + " seconds ago"
+            if second_diff < 120:
+                return "a minute ago"
+            if second_diff < 3600:
+                return str(second_diff / 60) + " minutes ago"
+            if second_diff < 7200:
+                return "an hour ago"
+            if second_diff < 86400:
+                return str(second_diff / 3600) + " hours ago"
+        if day_diff == 1:
+            return "Yesterday"
+        if day_diff < 7:
+            return str(day_diff) + " days ago"
+        if day_diff < 31:
+            return str(day_diff/7) + " weeks ago"
+        if day_diff < 365:
+            return str(day_diff/30) + " months ago"
+        return str(day_diff/365) + " years ago"
 
     from .tasks import celery
     celery.conf.update(app.config)
