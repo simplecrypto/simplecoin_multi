31c44844bfc551d9f0c027a738c18d90819cb9ac
Eric Cook <eric@simpload.com>
2014-03-02 20:34:03 -0600

    Fixes typo reporting higher than actual hashrate on user stats

--- static/js/graph.js
+++ static/js/graph.js
@@ -69,7 +69,7 @@ generate_graph = function(request_url, append_to) {
         ten_min += +data['points'][1440-h][1];
         h++;
     }
-    window.last_10min =ten_min/5
+    window.last_10min =ten_min/10
 
 
     data['points'].forEach(function(d,i) {
