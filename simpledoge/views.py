c26a4cba4b92dffbbf393db133dcac426a272814
Isaac Cook <isaac@simpload.com>
2014-03-09 23:48:04 -0500

    Fixed minor windowing problem with uncompressed overlap.

--- simpledoge/views.py
+++ simpledoge/views.py
@@ -196,14 +196,22 @@ def address_stats(address=None, window="hour"):
     # store all the raw data of we've grabbed
     workers = {}
 
-    def get_typ(typ):
+    def get_typ(typ, window=True):
+        """ Gets the latest slices of a specific size. window open toggles
+        whether we limit the query to the window size or not. We disable the
+        window when compressing smaller time slices because if the crontab
+        doesn't run we don't want a gap in the graph. This is caused by a
+        portion of data that should already be compressed not yet being
+        compressed. """
         # grab the correctly sized slices
+        base = db.session.query(typ).filter_by(user=address)
+        if window is False:
+            return base
         grab = typ.floor_time(datetime.datetime.utcnow()) - typ.window
-        return (db.session.query(typ).filter_by(user=address).
-                filter(typ.time >= grab))
+        return base.filter(typ.time >= grab)
 
     def compress_typ(typ):
-        for slc in get_typ(typ):
+        for slc in get_typ(typ, window=False):
             slice_dt = typ.upper.floor_time(slc.time)
             stamp = calendar.timegm(slice_dt.utctimetuple())
             workers.setdefault(slc.worker, {})
