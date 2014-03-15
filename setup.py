3e76d0c5116c9f0d7d0847a27e5b3ac96e8f81f8
Isaac Cook <isaac@simpload.com>
2014-03-15 13:23:27 -0500

    Version bump

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 from setuptools import setup, find_packages
 
 setup(name='simpledoge',
-      version='0.2.0',
+      version='0.3.0',
       description='Dogecoin mining with no registration required.',
       author='Eric Cook',
       author_email='eric@simpload.com',
