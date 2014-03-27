5883769fa384ffd74aa0be3c0d9347856be064ab
Eric Cook <eric@simpload.com>
2014-03-27 11:57:55 -0500

    switch default perc to donation perc in js.

--- static/js/utils.js
+++ static/js/utils.js
@@ -5,13 +5,13 @@ avg_shares_to_solve = function(difficulty){
 // Calculate estimated payout for next round
 round_payout = function(difficulty, user_shares, shares_to_solve, donate, round_reward, n_multiplier){
   user_percentage = user_shares / (shares_to_solve * n_multiplier);
-  return ((user_percentage * round_reward)) * (1 - donate);
+  return ((user_percentage * round_reward)) * (1 - (donate/100));
 }
 // Calculate est coins per day
 daily_est = function(last_10_shares, shares_to_solve, donate, round_reward) {
   day_shares = (last_10_shares * 6 * 24);
   daily_percentage = (day_shares / shares_to_solve);
-  return ((daily_percentage * round_reward)) * (1 - donate);
+  return ((daily_percentage * round_reward)) * (1 - (donate/100));
 }
 // Calculate number of shares in pplns
 shares_in_pplns = function(pplns_window, n_multiplier) {
