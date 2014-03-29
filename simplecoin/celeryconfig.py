847ecdbe111c0b25fb77d9044c51e2330ae641f4
Isaac Cook <isaac@simpload.com>
2014-03-28 21:04:20 -0500

    Changed task scheduling to use simplecoin.

--- simplecoin/celeryconfig.py
+++ simplecoin/celeryconfig.py
@@ -3,39 +3,39 @@ from datetime import timedelta
 
 CELERYBEAT_SCHEDULE = {
     'cache_user_donations': {
-        'task': 'simpledoge.tasks.cache_user_donation',
+        'task': 'simplecoin.tasks.cache_user_donation',
         'schedule': timedelta(minutes=15)
     },
     'compute_pplns': {
-        'task': 'simpledoge.tasks.update_pplns_est',
+        'task': 'simplecoin.tasks.update_pplns_est',
         'schedule': timedelta(minutes=15)
     },
     'compress_min_shares': {
-        'task': 'simpledoge.tasks.compress_minute',
+        'task': 'simplecoin.tasks.compress_minute',
         'schedule': timedelta(minutes=5),
     },
     'compress_five_min_shares': {
-        'task': 'simpledoge.tasks.compress_five_minute',
+        'task': 'simplecoin.tasks.compress_five_minute',
         'schedule': timedelta(minutes=60),
     },
     'general_cleanup': {
-        'task': 'simpledoge.tasks.general_cleanup',
+        'task': 'simplecoin.tasks.general_cleanup',
         'schedule': timedelta(minutes=120),
     },
     'update_block_state': {
-        'task': 'simpledoge.tasks.update_block_state',
+        'task': 'simplecoin.tasks.update_block_state',
         'schedule': timedelta(minutes=5),
     },
     'server_status': {
-        'task': 'simpledoge.tasks.server_status',
+        'task': 'simplecoin.tasks.server_status',
         'schedule': timedelta(minutes=2),
     },
     'check_worker_down': {
-        'task': 'simpledoge.tasks.check_down',
+        'task': 'simplecoin.tasks.check_down',
         'schedule': timedelta(minutes=1),
     },
     'update_diff_average': {
-        'task': 'simpledoge.tasks.difficulty_avg',
+        'task': 'simplecoin.tasks.difficulty_avg',
         'schedule': timedelta(hours=1),
     },
 }
