a3862123c0fe55fde356f3246640caa0e4a3d8ae
Isaac Cook <isaac@simpload.com>
2014-03-28 22:44:16 -0500

    Moved celery configs to a sub-config entry to isolate them more

--- simplecoin/celery_entry.py
+++ simplecoin/celery_entry.py
@@ -7,7 +7,7 @@ from flask import current_app
 app = create_app(celery=True)
 # import celerybeat settings
 celery.config_from_object('celeryconfig')
-celery.conf.update(app.config)
+celery.conf.update(app.config['celery'])
 
 with app.app_context():
     current_app.logger.info("Celery worker powering up... BBBVVVRRR!")
