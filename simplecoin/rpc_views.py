54720df9cde651e49f496cfdd3e458c9417757b5
Eric Cook <eric@simpload.com>
2014-07-26 14:36:19 -0500

    Refactor of rpc views to be generic for trade requests

--- simplecoin/rpc_views.py
+++ simplecoin/rpc_views.py
@@ -9,110 +9,111 @@ from . import db
 main = Blueprint('rpc', __name__)
 
 
-@main.route("/get_sell_requests", methods=['POST'])
-def get_sell_requests():
+@main.route("/get_trade_requests", methods=['POST'])
+def get_trade_requests():
     """ Used by remote procedure call to retrieve a list of sell requests to
     be processed. Transaction information is signed for safety. """
     s = TimedSerializer(current_app.config['rpc_signature'])
     args = s.loads(request.data)
-    current_app.logger.info("get_sell_requests being called, args of {}!".format(args))
+    current_app.logger.info("get_sell_requests being called, args of {}!".
+                            format(args))
     lock = False
     if isinstance(args, dict) and args['lock']:
         lock = True
 
-    sell_requests = TradeRequest.query.filter_by(_status=0, locked=False).all()
-    srs = [(sr.id, sr.currency, sr.quantity) for sr in sell_requests]
+    trade_requests = TradeRequest.query.filter_by(_status=0, locked=False).all()
+    trs = [(tr.id, tr.currency, tr.quantity, tr.type) for tr in trade_requests]
 
     if lock:
         current_app.logger.info("Locking sell requests at retriever request.")
-        for sr in sell_requests:
-            sr.locked = True
+        for tr in trade_requests:
+            tr.locked = True
         db.session.commit()
-    return s.dumps([srs, lock])
+    return s.dumps([trs, lock])
 
 
-@main.route("/update_sell_requests", methods=['POST'])
-def update_sell_requests():
-    """ Used as a response from an rpc sell request system. This will update the amount received for
-    a sell request and its status. Both request and response are signed. """
+@main.route("/update_trade_requests", methods=['POST'])
+def update_trade_requests():
+    """ Used as a response from an rpc sell request system. This will update
+    the amount received for a sell request and its status. Both request and
+    response are signed. """
     s = TimedSerializer(current_app.config['rpc_signature'])
     data = s.loads(request.data)
 
     # basic checking of input
     try:
-        assert 'completed_srs' in data
-        assert isinstance(data['completed_srs'], dict)
-        for sr_id, quantity in data['completed_srs'].iteritems():
-            assert isinstance(sr_id, basestring)
-            assert isinstance(quantity, int)
+        assert 'completed_trs' in data
+        assert isinstance(data['completed_trs'], dict)
     except AssertionError:
-        current_app.logger.warn("Invalid data passed to update_sell_requests", exc_info=True)
+        current_app.logger.warn("Invalid data passed to update_sell_requests",
+                                exc_info=True)
         abort(400)
 
-    for ts_id, quantity in data['completed_srs'].iteritems():
-        ts_id = int(ts_id)
-        ts = (TradeRequest.query.filter_by(id=ts_id). with_lockmode('update').one())
-        if not ts.payouts:
+    for tr_id, quantity in data['completed_trs'].iteritems():
+        tr_id = int(tr_id)
+        tr = (TradeRequest.query.filter_by(id=tr_id).with_lockmode('update').one())
+        tr.exchanged_quantity = quantity
+        tr._status = 6
+
+        if not tr.payouts:
             raise Exception("Trade request has no attached payouts")
-        ts.exchanged_quantity = quantity
-        ts._status = 6
+        if tr.payouts:
+            # truncated delivery based on the payouts percentage of the total
+            # exchanged value
+            distributed = 0
+            for payout in tr.payouts:
+                if tr.type == "sell":
+                    assert payout.sell_amount is None
+                    payout.sell_amount = float(
+                        payout.amount * tr.exchanged_quantity) // tr.quantity
+                    distributed += payout.sell_amount
+                elif tr.type == "buy":
+                    assert payout.buy_amount is None
+                    payout.buy_amount = float(
+                        payout.sell_amount * tr.exchanged_quantity) // tr.quantity
+                    distributed += payout.buy_amount
+                    payout.payable = True
+                else:
+                    raise AttributeError("Invalid tr type")
 
-        # truncated delivery based on the payouts percentage of the total
-        # exchanged value
-        distributed = 0
-        for payout in ts.payouts:
-            if ts.type == "sell":
-                assert payout.sell_amount is None
-                payout.sell_amount = float(
-                    payout.amount * ts.exchanged_quantity) // ts.quantity
-                distributed += payout.sell_amount
-            elif ts.type == "buy":
-                assert payout.buy_amount is None
-                payout.buy_amount = float(
-                    payout.sell_amount * ts.exchanged_quantity) // ts.quantity
-                distributed += payout.buy_amount
-                payout.payable = True
-            else:
-                raise AttributeError("Invalid tr type")
+            current_app.logger.info(
+                "Total accrued after trunated iteration {:,}; {}%"
+                .format(distributed / 100000000.0, (distributed / float(tr.exchanged_quantity)) * 100))
 
-        current_app.logger.info(
-            "Total accrued after trunated iteration {:,}; {}%"
-            .format(distributed / 100000000.0, (distributed / float(ts.exchanged_quantity)) * 100))
+            # round robin distribution of the remaining fractional satoshi
+            i = 0
+            while distributed < tr.exchanged_quantity:
+                for payout in tr.payouts:
+                    if tr.type == "sell":
+                        payout.sell_amount += 1
+                    elif tr.type == "buy":
+                        payout.buy_amount += 1
+                    distributed += 1
+                    i += 1
 
-        # round robin distribution of the remaining fractional satoshi
-        i = 0
-        while distributed < ts.exchanged_quantity:
-            for payout in ts.payouts:
-                if ts.type == "sell":
-                    payout.sell_amount += 1
-                elif ts.type == "buy":
-                    payout.buy_amount += 1
-                distributed += 1
-                i += 1
+                    # exit if we've exhausted
+                    if distributed >= tr.exchanged_quantity:
+                        break
 
-                # exit if we've exhausted
-                if distributed >= ts.exchanged_quantity:
-                    break
+            current_app.logger.info("Ran round robin distro {} times to finish "
+                                    "distrib".format(i))
 
-        current_app.logger.info("Ran round robin distro {} times to finish "
-                                "distrib".format(i))
+            # double check complete and exact distribution
+            if tr.type == "sell":
+                assert sum([p.sell_amount for p in tr.payouts]) == tr.exchanged_quantity
+            elif tr.type == "buy":
+                assert sum([p.buy_amount for p in tr.payouts]) == tr.exchanged_quantity
 
-        # double check complete and exact distribution
-        if ts.type == "sell":
-            assert sum([p.sell_amount for p in ts.payouts]) == ts.exchanged_quantity
-        elif ts.type == "buy":
-            assert sum([p.buy_amount for p in ts.payouts]) == ts.exchanged_quantity
-
-        current_app.logger.info(
-            "Successfully pushed trade result for request id {:,} and amount {:,} to {:,} payouts."
-            .format(ts.id, ts.exchanged_quantity / 100000000.0, len(ts.payouts)))
+            current_app.logger.info(
+                "Successfully pushed trade result for request id {:,} and amount {:,} to {:,} payouts."
+                .format(tr.id, tr.exchanged_quantity / 100000000.0, len(tr.payouts)))
 
     db.session.commit()
     return s.dumps(dict(success=True, result="Trade requests successfully updated."))
 
 
-@main.route("/reset_sell_requests", methods=['POST'])
-def reset_sell_requests():
+@main.route("/reset_trade_requests", methods=['POST'])
+def reset_trade_requests():
     """ Used as a response from an rpc sell request system. This will reset
     the locked status of a list of sell requests upon failure on the remote
     side. Both request and response are signed. """
@@ -123,36 +124,22 @@ def reset_sell_requests():
     try:
         assert 'reset' in data
         assert isinstance(data['reset'], bool)
-        assert isinstance(data['sr_ids'], list)
-        for id in data['sr_ids']:
+        assert isinstance(data['tr_ids'], list)
+        for id in data['tr_ids']:
             assert isinstance(id, int)
     except AssertionError:
-        current_app.logger.warn("Invalid data passed to reset_sell_requests", exc_info=True)
+        current_app.logger.warn("Invalid data passed to reset_trade_requests",
+                                exc_info=True)
         abort(400)
 
-    if data['reset'] and data['sr_ids']:
-        srs = TradeRequest.query.filter(TradeRequest.id.in_(data['sr_ids'])).all()
+    if data['reset'] and data['tr_ids']:
+        srs = TradeRequest.query.filter(TradeRequest.id.in_(data['tr_ids'])).all()
         for sr in srs:
             sr.locked = False
         db.session.commit()
         return s.dumps(dict(success=True, result="Successfully reset"))
 
     return s.dumps(True)
-
-
-@main.route("/get_final_payouts", methods=['GET'])
-def get_final_payouts():
-    """ Used by remote procedure call to retrieve a list of final payouts to
-    be processed. Information is signed for safety. """
-    s = TimedSerializer(current_app.config['rpc_signature'])
-    args = s.loads(request.data)
-    current_app.logger.info("get_final_payouts being called, args of {}!".format(args))
-
-    final_payouts = []
-
-    fps = [(fp.id, fp.quantity, fp.desired_currency) for fp in final_payouts]
-
-    return s.dumps([fps])
 
 
 @main.route("/get_payouts", methods=['POST'])
