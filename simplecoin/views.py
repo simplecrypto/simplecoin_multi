606f11f89d17d217815bfcf3ee55475e298da156
Eric Cook <eric@simpload.com>
2014-08-13 16:15:51 -0500

    Handle invalid addresses correctly. Doh.

--- simplecoin/views.py
+++ simplecoin/views.py
@@ -156,7 +156,10 @@ def worker_stats(address=None, worker=None, stat_type=None, window="hour"):
 @main.route("/<address>")
 def user_dashboard(address=None):
     # Do some checking to make sure the address is valid + payable
-    curr = check_valid_currency(address).key
+    curr = check_valid_currency(address)
+    if curr:
+        curr = curr.key
+
     allowed_currencies = valid_currencies()
     if len(address) != 34 or not curr or curr not in allowed_currencies:
         return render_template('invalid_address.html',
