42e6094178780b9579699dfa7d76c9f430fa1f02
Isaac Cook <isaac@simpload.com>
2014-09-03 18:44:13 -0500

    Update init.sql

--- util/init.sql
+++ util/init.sql
@@ -1,8 +1,9 @@
-DROP DATABASE IF EXISTS simplecoin;
-DROP DATABASE IF EXISTS simplecoin_testing;
 CREATE USER simplecoin WITH PASSWORD 'testing';
+-- Production database
+DROP DATABASE IF EXISTS simplecoin;
 CREATE DATABASE simplecoin;
 GRANT ALL PRIVILEGES ON DATABASE simplecoin to simplecoin;
 -- Create a testing database to be different than dev
+DROP DATABASE IF EXISTS simplecoin_testing;
 CREATE DATABASE simplecoin_testing;
 GRANT ALL PRIVILEGES ON DATABASE simplecoin_testing to simplecoin;
